188ea7bb28c5a291a33401f993c4ad72e94d958e
Benjamin Rush <ben@ben-rush.net>
2012-11-25 10:53:45 -0600

    Addition of the phaser files

---
+++ ADAFiles/mged_data_phaser/messages.adb
@@ -0,0 +1,136 @@
+with
+Ada.Calendar,
+     Ada.Strings,
+     Ada.Strings.Maps,
+     Ada.Strings.Fixed;
+use
+  Ada.Calendar,
+    Ada.Strings,
+    Ada.Strings.Maps,
+    Ada.Strings.Fixed;
+
+package body Messages is
+
+   package Iio is new Integer_Io (Integer); use Iio;
+
+   type Month_Name_Array is array (1 .. 12) of String (1 .. 3);
+   Month_Name : constant Month_Name_Array :=
+                  ( 1 => "Jan",  2 => "Feb",  3 => "Mar",  4 => "Apr",
+                    5 => "May",  6 => "Jun",  7 => "Jul",  8 => "Aug",
+                    9 => "Sep", 10 => "Oct", 11 => "Nov", 12 => "Dec");
+   Yr : Year_Number;
+   Mo : Month_Number;
+   Dy : Day_Number;
+   Hr, Mn : Integer;
+   Sc : Day_Duration;
+   S : String (1 .. 2);
+
+   --  This local procedure breaks the "source" string into lines at
+   --  word boundaries.  The maximum line length is specifed by "width".
+   --  The text will be written to the "Dest" file (which should be
+   --  open).
+   procedure Justify_Text
+     (Source : in String;
+      Width  : in Positive;
+      Dest   : in File_Type) is
+      Delimiter                      : Character_Set;
+      First, Last, Tail, String_Size : Integer;
+      Done                           : Boolean;
+   begin
+      Delimiter := To_Set (" ");
+      Done := False;
+      Last := Source'First - 1;
+      Tail := Source'Last;
+      String_Size := 0;
+      --  Find each token in the source string
+      while not Done loop
+         Find_Token (Source (Last + 1 .. Tail), Delimiter, Outside,
+                     First, Last);
+         if Last = 0 then
+            Done := True;
+         else
+            --  Write the token as long as the length of the
+            --  output line will be less than "width". If it
+            --  is greater than width, go to a new line and
+            --  then write the token.
+            String_Size := String_Size + (Last - First + 2);
+            if String_Size - 1 > Width then
+               New_Line (Dest);
+               Put (Dest, Source (First .. Last) & " ");
+               String_Size := (Last - First + 2);
+            else
+               Put (Dest, Source (First .. Last) & " ");
+            end if;
+         end if;
+      end loop;
+   end Justify_Text;
+
+   procedure Error_Msg (Procedure_Name : in String;
+                        Message        : in String;
+                        Dest           : in File_Type := Standard_Error) is
+   begin
+      Split ((Clock + Day_Duration (7 * 3600)), Yr, Mo, Dy, Sc);
+      Hr := Integer (Sc) / 3600;
+      Mn := (Integer (Sc) - (3600 * Hr)) / 60;
+      Sc := Day_Duration (Integer (Sc) - (3600 * Hr) - (60 * Mn));
+      Put (Dest, "***ERROR***  ");
+      Put (Dest, Integer (Yr), 4);
+      Put (Dest, " " & Month_Name (Integer (Mo)) & " ");
+      Put (S, Integer (Dy));
+      if Dy < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put (Dest, S & " ");
+      Put (S, Hr);
+      if Hr < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put (Dest, S & ":");
+      Put (S, Mn);
+      if Mn < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put (Dest, S & ":");
+      Put (S, Integer (Sc));
+      if Integer (Sc) < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put_Line (Dest, S & " UT (" & Procedure_Name & ") ");
+      Justify_Text (Trim (Message, Both), 60, Dest);
+   end Error_Msg;
+
+   procedure Status_Msg (Procedure_Name : in String;
+                         Message        : in String;
+                         Dest           : in File_Type := Standard_Output) is
+   begin
+      Split ((Clock + Day_Duration (7 * 3600)), Yr, Mo, Dy, Sc);
+      Hr := Integer (Sc) / 3600;
+      Mn := (Integer (Sc) - (3600 * Hr)) / 60;
+      Sc := Day_Duration (Integer (Sc) - (3600 * Hr) - (60 * Mn));
+      Put (Dest, "***STATUS*** ");
+      Put (Dest, Integer (Yr), 4);
+      Put (Dest, " " & Month_Name (Integer (Mo)) & " ");
+      Put (S, Integer (Dy));
+      if Dy < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put (Dest, S & " ");
+      Put (S, Hr);
+      if Hr < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put (Dest, S & ":");
+      Put (S, Mn);
+      if Mn < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put (Dest, S & ":");
+      Put (S, Integer (Sc));
+      if Integer (Sc) < 10 then
+         S (1 .. 1) := "0";
+      end if;
+      Put_Line (Dest, S & " UT (" & Procedure_Name & ") ");
+      Justify_Text (Trim (Message, Both), 60, Dest);
+   end Status_Msg;
+
+end Messages;
---
+++ ADAFiles/mged_data_phaser/mged_data_phaser.adb
@@ -0,0 +1,143 @@
+--  <description>
+--  This procedure "phases" the Julian dates in a TrES data file
+--  (or a PDM file) so that the dates become time normalized to the
+--  putative period. It produces an output file which contains the
+--  same information as the input file except that the Julian dates
+--  are re-written in the normalized phased form.
+--
+--  The procedure requires three command line arguments. The first is
+--  the name of the input file. The input file should be the output from
+--  the TrES data web site, http://asteroid.lowell.edu/LARI/mged.html.
+--  The header and trailer lines of the raw input file should be removed.  Note
+--  that any text file can be used if the first token in each line is
+--  the Julian date of the datum. That is, this procedure should work
+--  on some of the PDM output files.
+--
+--  The second argument is the name of the output file. The output file
+--  format is identical to the input file format except that the JD is
+--  converted to a normalized, phased time.
+--
+--  The third argument, a real number, is the putative period of the
+--  stellar lightcurve. It is normally expressed in days since the
+--  input time is a Julian date (also days).
+--  </description>
+with
+Ada.Text_Io,
+     Ada.Command_Line,
+     Ada.Strings,
+     Ada.Strings.Fixed,
+     Ada.Strings.Maps,
+     Messages;
+use
+  Ada.Text_Io,
+    Ada.Command_Line,
+    Ada.Strings,
+    Ada.Strings.Fixed,
+    Ada.Strings.Maps,
+    Messages;
+
+procedure Mged_Data_Phaser is
+   package Lfio is new Float_Io (Long_Float); use Lfio;
+   Period, Time, Phased_Time : Long_Float;
+   Tail, Tail1, First, Last, Line_Count : Natural;
+   Src, Dst : File_Type;
+   Buffer : String (1 .. 512);
+   Delimiter : Character_Set;
+   User_Error : exception;
+
+   --  The "mod" function is a generalization of the "remainder" function.
+   --  This implementation is an "overload" of the mod funtion for
+   --  integer input.
+   function "mod"
+     (Arg1, Arg2 : Long_Float) return Long_Float is
+   begin
+      return (Arg1 - (Arg2 * Long_Float'Floor (Arg1 / Arg2)));
+   end "mod";
+
+begin
+   --  Verify the correct number of command line arguments are present.
+   if Argument_Count /= 3 then
+      Error_Msg ("mged_data_phaser",
+                 "This procedure requires exactly three " &
+                   "command line arguments. The first is the source data " &
+                   "file name. The second is the output file name for " &
+                   "the result file. The third is the period expressed " &
+                   "in days. The procedure did not find three command " &
+                   "line arguments.");
+      raise User_Error;
+   end if;
+   --  Verify the source file is present and, if so, open it.
+   begin
+      Open (Src, In_File, Argument (1));
+   exception
+      when others =>
+         Error_Msg ("mged_data_phaser",
+                    "The procedure could not open the specified input file. " &
+                      "Often, this error occurs when the file name is " &
+                      "misspelled or the file does not exist.");
+         raise User_Error;
+   end;
+   --  Convert the period (third command line argument) from string
+   --  format to long_float format.
+   begin
+      Get (Argument (3), Period, Tail);
+   exception
+      when others =>
+         Error_Msg ("mged_data_phaser",
+                    "The procedure could not convert the third command " &
+                      "line argument, the period, into a number.");
+         raise User_Error;
+   end;
+   --  Open the output (destination) file.
+   begin
+      Create (Dst, Out_File, Argument (2));
+   exception
+      when others =>
+         Error_Msg ("mged_data_phaser",
+                    "The procedure could not open the specified output " &
+                      "file. Often this error occurs when the user does " &
+                      "not have write privleges in the destination " &
+                      "directory.");
+         raise User_Error;
+   end;
+   First := Buffer'First;
+   Delimiter := To_Set (", ");
+   Line_Count := 0;
+   --  Loop through each line of the input file.
+   while not End_Of_File (Src) loop
+      --  Read the line and get the first token, under the assumption
+      --  that the token in the JD of the observation.
+      Get_Line (Src, Buffer, Tail);
+      Line_Count := Line_Count + 1;
+      Find_Token (Buffer (First .. Tail), Delimiter, Outside,
+                  First, Last);
+      --  Convert the JD token from string format to long_float format.
+      begin
+         Get (Buffer (First .. Last), Time, Tail1);
+      exception
+         when others =>
+            Error_Msg ("mged_data_phaser",
+                       "The procedure could not convert a Julian date, " &
+                         "from the input file, into a number. The " &
+                         "offending string is """ & Buffer (First .. Last) &
+                         """.");
+            raise User_Error;
+      end;
+      --  Convert the JD into phased, normalized time and write the source
+      --  line into the output file with the JD replaced with the phased
+      --  time.
+      Phased_Time := (Time mod Period) / Period;
+      Put (Dst, Phased_Time, 4, 8, 0); Put_Line (Dst, Buffer (Last + 1 .. Tail));
+   end loop;
+   if Line_Count = 0 then
+      Error_Msg ("mged_data_phaser",
+                 "No data were found in the input file. " &
+                   "Your output file will be empty.");
+   elsif Line_Count = 1 then
+      Status_Msg ("mged_data_phaser",
+                   "Only one datum was found in the input file. The " &
+                     "output file will be unexpectedly short.");
+   end if;
+   Close (Dst);
+   Close (Src);
+end Mged_Data_Phaser;

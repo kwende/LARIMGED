cb3d6d5d6d4f206a0ec0338f261cca53af56151d
Ben Rush <ben@ben-rush.net>
2013-01-09 23:01:24 -0600

    Commit to work machine
    Potentially breaking change just to move stuff to work.

---
+++ Visual Studio/FFTW/FFTWViewer/GraphHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Visiblox.Charts;
+
+namespace FFTWViewer
+{
+    public class GraphData
+    {
+        public Brush Brush { get; set; }
+        public double[] Data { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class GraphHandler
+    {
+        public Chart TheChart { get; private set; }
+
+        public GraphHandler(Chart theChart)
+        {
+            TheChart = theChart; 
+        }
+
+        public void Update(List<GraphData> dataList)
+        {
+            double largestY = 0.0;
+            double smallestY = double.MaxValue; 
+
+            TheChart.Series.Clear();
+            for(int c=0;c<dataList.Count;c++)
+            {
+                GraphData graphData = dataList[c];
+
+                double[] data = graphData.Data;
+
+                TheChart.Series.Add(new LineSeries { LineStroke = graphData.Brush });
+
+                DataSeries<double, double> points = new DataSeries<double, double>(graphData.Name);
+                for (int x = 0; x < data.Length; x++)
+                {
+                    double y = data[x];
+                    if (y > largestY)
+                    {
+                        largestY = y; 
+                    }
+                    if (y < smallestY)
+                    {
+                        smallestY = y; 
+                    }
+                    points.Add(new DataPoint<double, double> { X = x, Y = y });
+                }
+                TheChart.Series[c].DataSeries = points;
+            }
+
+            TheChart.Series[0].YAxis.Range = new DoubleRange(smallestY, largestY);
+        }
+    }
+}
--- Visual Studio/FFTW/FFTWViewer/MainWindow.xaml.cs
+++ Visual Studio/FFTW/FFTWViewer/MainWindow.xaml.cs
@@ -41,22 +41,6 @@ namespace FFTWViewer
                 FFTW fftw = new FFTW();
                 double[] output = fftw.Process(data, maxToProcess);
 
-                DataSeries<double, double> xLiftData = new DataSeries<double, double>("Input");
-                for (int x = 0; x < output.Length; x++)
-                {
-                    xLiftData.Add(new DataPoint<double, double> { X = x, Y = data[x] });
-                }
-                exampleChart.Series[0].DataSeries = xLiftData;
-
-                DataSeries<double, double> xLiftData2 = new DataSeries<double, double>("Output");
-                for (int x = 0; x < data.Length; x++)
-                {
-                    xLiftData2.Add(new DataPoint<double, double> { X = x, Y = output[x] });
-                }
-                exampleChart.Series[1].DataSeries = xLiftData2;
-
-                exampleChart.Series[0].YAxis.Range = new DoubleRange(smallest, largest);
-
             }
         }
 
@@ -92,6 +76,11 @@ namespace FFTWViewer
             DoIt(_data, (int)slider.Value);
             TextBox_Value.Text = ((int)slider.Value).ToString(); 
             return; 
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
